b0a38b07132309363c2b8ab108d95e0c1d12df36
ncockcro <35208141+ncockcro@users.noreply.github.com>
2018-05-14 13:50:29 -0400

    Completed Lab 12
    Full implementation for make a GUI application which calculates the primes numbers up to a user specified number. It also uses multi threading so while the application is doing the calculations, the GUI is still responsive.

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Lab12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.inputLabel = new System.Windows.Forms.Label();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(241, 98);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(278, 31);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(39, 82);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(141, 63);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(567, 82);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(125, 71);
+            this.cancelButton.TabIndex = 2;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            this.cancelButton.KeyDown += new System.Windows.Forms.KeyEventHandler(this.cancel);
+            // 
+            // inputLabel
+            // 
+            this.inputLabel.AutoSize = true;
+            this.inputLabel.Location = new System.Drawing.Point(266, 44);
+            this.inputLabel.Name = "inputLabel";
+            this.inputLabel.Size = new System.Drawing.Size(239, 25);
+            this.inputLabel.TabIndex = 3;
+            this.inputLabel.Text = "Enter a positive integer:";
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(241, 347);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.Size = new System.Drawing.Size(234, 117);
+            this.outputTextBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(325, 266);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Results";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 623);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.inputLabel);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "Form1";
+            this.Text = "Lab 12";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.form1_paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Label inputLabel;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿/*
+ * Written by: Nicholas Cockcroft
+ * Date: April 27, 2018
+ * Course: .NET Environment
+ * Assignment: Lab 12
+ * 
+ * Description: Make a gui application where a user enters a number and have a thread
+ * calculate how many numbers are there between 2 and the user specified number. While
+ * doing this, the gui should still be responsive.
+ * 
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading; // Threads
+
+namespace Lab12
+{
+    public partial class Form1 : Form
+    {
+        // Global variables
+        int maxPrimeNum;
+        private static int[] mt = new int[2];
+        int totalPrimeNum = 0;
+        Thread[] thread = new Thread[5];
+        public Form1()
+        {
+            InitializeComponent();
+            cancelButton.Enabled = false; // Initialize the cancel button to false since there is nothing to cancel
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            // If the button is enabled, then call the thread to calculate prime numbers
+            if(startButton.Enabled == true && inputTextBox.Text != "")
+            {
+                // Checking to make sure it is a valid number that was entered
+                try
+                {
+                    maxPrimeNum = int.Parse(inputTextBox.Text);
+                }
+                catch(Exception)
+                {
+                    MessageBox.Show("Invalid maximum number.");
+                    return;
+                }
+
+                // Call the thread to calculate the prime numbers
+                initThreads();
+
+                // Set it so the user can't press start again but they can cancel the operation
+                startButton.Enabled = false;
+                cancelButton.Enabled = true;
+
+            }
+            // Otherwise, just return
+            return;
+        }
+
+        private void form1_paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        // Initializes a thread to calculate prime numbers
+        private void initThreads()
+        {
+            thread[0] = new Thread(threadFunc);
+            thread[0].Start();
+        }
+
+        // Thread function for execution of the thread
+        private void threadFunc()
+        {
+            // Intializae variables to keep track of the current number and a counter for how many numbers
+            // the current number is divisible by
+            int primeNum = 0;
+            int primeCounter = 0;
+            totalPrimeNum = 0;
+
+
+            // Going through the first 1,000,000 prime numbers...
+            for (int i = 2; i < maxPrimeNum; i++)
+            {
+                primeNum = i;
+
+                // Go through and tyy to divide every number from 1 to the current prime
+                // number and keep track of how many times it was successfully divided
+                for (int j = 1; j <= i; j++)
+                {
+                    if (primeNum % j == 0)
+                    {
+                        primeCounter++;
+                    }
+                }
+                // If the counter was less than or equal to 2, then that number was prime
+                if (primeCounter <= 2)
+                {
+                    lock (mt)
+                    {
+                        totalPrimeNum++;
+                    }
+                }
+                primeCounter = 0;
+            }
+
+            // Once the calculations are over, restore access to start button and restrict access to cancel button
+            startButton.Enabled = true;
+            cancelButton.Enabled = false;
+
+            // Output the number of prime numbers to the output text box
+            outputTextBox.Text = totalPrimeNum.ToString() + " prime numbers";
+        }
+
+        // Cancel button which kills the thread and enables the start button and disables the cancel button
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            thread[0].Abort();
+            outputTextBox.Text = "";
+            startButton.Enabled = true;
+            cancelButton.Enabled = false;
+        }
+
+        private void cancel(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
